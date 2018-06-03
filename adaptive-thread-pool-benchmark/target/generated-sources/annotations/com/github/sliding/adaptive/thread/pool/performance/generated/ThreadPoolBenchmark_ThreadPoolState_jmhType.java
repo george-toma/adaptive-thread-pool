f82a01be212981835b5cb5f421a60602880b07d9
george-toma <toma.george89@yahoo.com>
2018-06-03 22:41:22 +0300

    Removed wrong /target directory

--- adaptive-thread-pool-benchmark/target/generated-sources/annotations/com/github/sliding/adaptive/thread/pool/performance/generated/ThreadPoolBenchmark_ThreadPoolState_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.github.sliding.adaptive.thread.pool.performance.generated;
-public class ThreadPoolBenchmark_ThreadPoolState_jmhType extends ThreadPoolBenchmark_ThreadPoolState_jmhType_B3 {
-}
-
--- adaptive-thread-pool-benchmark/target/generated-sources/annotations/com/github/sliding/adaptive/thread/pool/performance/generated/ThreadPoolBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.github.sliding.adaptive.thread.pool.performance.generated;
-public class ThreadPoolBenchmark_jmhType extends ThreadPoolBenchmark_jmhType_B3 {
-}
-
